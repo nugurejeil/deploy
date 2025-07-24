33bcf6cf455ddeee988fc978037e48d7ef16f302
nugurejeil <nugurejeil1@gmail.com>
2025-07-24 23:10:53 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+    return (
+        <div className="App">
+            <h1>나의 첫 React 배포! 🎉</h1>
+            <p>Vite + React로 만든 웹사이트입니다.</p>
+        </div>
+    )
+}
+
+export default App
