9098fb12021029cc12e008db85f3744329199367
nugurejeil <nugurejeil1@gmail.com>
2025-07-24 23:46:29 +0900

    새로운 내용 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
         <div className="App">
             <h1>나의 첫 React 배포! 🎉</h1>
             <p>Vite + React로 만든 웹사이트입니다.</p>
+            <h2>내용이 추가되었습니다!!!</h2>
         </div>
     )
 }
